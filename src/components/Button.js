3bbbecd326ae2ae0268af4cbf67edce310845196
Paul Murray <paulmurray85@gmail.com>
2021-02-07 20:07:32 -0800

    debugged button issue

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import "./Button.css";
 import { Link } from "react-router-dom";
 
 const STYLES = ["btn--primary", "btn--outline"];
-const SIZES = ["bttn--medium", "btn--large"];
+const SIZES = ["btn--medium", "btn--large"];
 
 export const Button = ({
   children,
@@ -16,7 +16,7 @@ export const Button = ({
     ? buttonStyle
     : STYLES[0];
 
-  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : STYLES[0];
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
   return (
     <Link to="/sign-up" className="btn-mobile">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
-            TRVLR <i className="fab fa-typo3" />
+          TRVLR <i className="fab fa-typo3" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
