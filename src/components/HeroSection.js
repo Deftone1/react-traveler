6b868055c0a277ec400b958ebe7d8d0942b73f49
Paul Murray <paulmurray85@gmail.com>
2021-02-07 22:45:08 -0800

    hero page complete continue resolving video bugs

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Button } from "./Button";
+import "./HeroSection.css";
+import "../App.css";
 
 function HeroSection() {
-    return (
-        <div className="hero-container">
-            <video src="/src/videos/video-2.mp4" autoPlay loop muted />
-            <h1>ADVENTURE AWAITS</h1>
-            <p>What are you waiting for?</p>
-            <div className="hero-btns">
-                
-            </div>
-            
-        </div>
-    )
+  return (
+    <div className="hero-container">
+        {/* CANNOT GET THE VIDEO WORKING! */}
+      {/* <video src="/videos/video-2.mp4" autoPlay loop muted /> */}
+
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          WATCH TRAILER
+          <i className="far fa-play-circle" />
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
