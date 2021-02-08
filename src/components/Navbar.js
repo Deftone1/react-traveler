c449f03aec833f8ec3ce7d0b1702fbbb69c71ceb
Paul Murray <paulmurray85@gmail.com>
2021-02-07 21:41:16 -0800

    initial hero page framework

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { Button } from "./Button.js";
@@ -18,19 +18,23 @@ function Navbar() {
     }
   };
 
+  useEffect(() => {
+    showButton()
+  }, []);
+
   window.addEventListener('resize', showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-          TRVLR <i className="fab fa-typo3" />
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+          TRAVELER <i className="fab fa-typo3" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
-          <ul className={click ? "nav-menu-active" : "nav-menu"}>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
           <li className="nav-item">
             <Link to="/" className="nav-links" onClick={closeMobileMenu}>
               Home
